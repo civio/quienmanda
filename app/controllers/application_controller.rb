f2976a9b13f9085369dd9bed9c77388868e8826b
David Cabo <david.cabo@gmail.com>
2013-08-01 04:19:54 +0200

    Add user name to sign-in/up forms. Closes #11

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # Add extra parameters to the basic Devise user registration
+  # See https://github.com/plataformatec/devise#strong-parameters
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
   # Handle unauthorized access to the admin interface
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to '/', alert: exception.message
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password) }
   end
 end
