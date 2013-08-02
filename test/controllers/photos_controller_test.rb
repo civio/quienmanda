5c5d456ec46f01541fb3e4dfedf0514368ed8132
David Cabo <david.cabo@gmail.com>
2013-08-02 03:16:53 +0200

    Fix tests

--- test/controllers/photos_controller_test.rb
+++ test/controllers/photos_controller_test.rb
@@ -10,40 +10,4 @@ class PhotosControllerTest < ActionController::TestCase
     assert_response :success
     assert_not_nil assigns(:photos)
   end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create photo" do
-    assert_difference('Photo.count') do
-      post :create, photo: {  }
-    end
-
-    assert_redirected_to photo_path(assigns(:photo))
-  end
-
-  test "should show photo" do
-    get :show, id: @photo
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @photo
-    assert_response :success
-  end
-
-  test "should update photo" do
-    patch :update, id: @photo, photo: {  }
-    assert_redirected_to photo_path(assigns(:photo))
-  end
-
-  test "should destroy photo" do
-    assert_difference('Photo.count', -1) do
-      delete :destroy, id: @photo
-    end
-
-    assert_redirected_to photos_path
-  end
 end
