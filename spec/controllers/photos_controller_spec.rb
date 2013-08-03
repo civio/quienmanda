638d3c02164ed1cfec68b4336df821fdc940c33c
David Cabo <david.cabo@gmail.com>
2013-08-03 19:57:55 +0200

    Add basic tests for Photo controller

---
+++ spec/controllers/photos_controller_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe PhotosController do
+  fixtures :photos
+
+  context "an anon user" do
+    let(:public_photo) { photos(:public_photo) }
+
+    it "sees the list of published people" do
+      get :index
+      assert_template :index
+      assigns(:photos).should == [public_photo]
+    end
+
+    it "sees a published photo" do
+      get :show, id: 'a-public-photo'
+      assert_template :show
+      assigns(:photo).should == public_photo
+    end
+
+    it "doesn't see an unpublished photo" do
+      get :show, id: 'a-private-photo'
+      response.should redirect_to('/')
+      flash[:alert].should == "You are not authorized to access this page."
+    end
+  end
+
+  context "a normal user" do
+    it "still doesn't see an unpublished photo" do
+      sign_in create(:user)
+      get :show, id: 'a-private-photo'
+      response.should redirect_to('/')
+      flash[:alert].should == "You are not authorized to access this page."
+    end
+  end
+
+  context "an admin" do
+    it "sees an unpublished photo" do
+      sign_in create(:admin)
+      get :show, id: 'a-private-photo'
+      assert_template :show
+    end
+  end
+end
