39499af389b50f726cdc1d173d383f32e29d4af8
David Cabo <david.cabo@gmail.com>
2013-08-15 14:33:46 +0200

    Show imported/matched data side by side

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -6,42 +6,44 @@ class ImportController < ApplicationController
   # TODO: For now we'll just try importing all the data. Should pick only a 
   # certain job, and filter those records already imported successfully.
   def index
-    @found_entities = {}
-    @missing_entities = {}
-    @found_relation_types = {}
-    @missing_relation_types = {}
+    @entities = {}
+    @relation_types = {}
     Fact.all.each do |fact|
-      # Try to find an existing RelationType matching the imported data
+      # Check whether we've seen this datum before
       role = fact.properties['Cargo']
-      relation_type = RelationType.find_by(["lower(description) = ?", role.downcase])
+      if @relation_types[role]
+        @relation_types[role][:count] += 1
 
-      # Keep track of success/failures
-      end_bucket = (relation_type) ? @found_relation_types : @missing_relation_types
-      end_bucket[role] = (end_bucket[role] || 0) + 1
+      else  # Try to find an existing RelationType matching the imported data
+        relation_type = RelationType.find_by(["lower(description) = ?", role.downcase])
+        @relation_types[role] = { count: 1, relation_type: relation_type }
+      end
 
 
-      # Try to find an existing Entity matching the imported data
+      # Check whether we've seen this datum before
       source = fact.properties['Nombre']
-      source_entity = Entity.find_by(["lower(name) = ?", source.downcase])
+      if @entities[source]
+        @entities[source][:count] += 1
 
-      # Keep track of success/failures
-      end_bucket = (source_entity) ? @found_entities : @missing_entities
-      end_bucket[source] = (end_bucket[source] || 0) + 1
+      else  # Try to find an existing Entity matching the imported data
+        relation_type = Entity.find_by(["lower(name) = ?", source.downcase])
+        @entities[source] = { count: 1, relation_type: relation_type }
+      end
 
 
-      # Try to find an existing Entity matching the imported data
+      # Check whether we've seen this datum before
       target = fact.properties['Empresa']
-      target_entity = Entity.find_by(["lower(name) = ?", target.downcase])
+      if @entities[target]
+        @entities[target][:count] += 1
 
-      # Keep track of success/failures
-      end_bucket = (target_entity) ? @found_entities : @missing_entities
-      end_bucket[target] = (end_bucket[target] || 0) + 1
+      else  # Try to find an existing Entity matching the imported data
+        relation_type = Entity.find_by(["lower(name) = ?", target.downcase])
+        @entities[target] = { count: 1, relation_type: relation_type }
+      end
     end
 
-    @found_entities = @found_entities.to_a.sort_by {|e| -e[1]}
-    @missing_entities = @missing_entities.to_a.sort_by {|e| -e[1]}
-    @found_relation_types = @found_relation_types.to_a.sort_by {|e| -e[1]}
-    @missing_relation_types = @missing_relation_types.to_a.sort_by {|e| -e[1]}
+    @entities = @entities.to_a.sort_by {|e| -e[1][:count]}
+    @relation_types = @relation_types.to_a.sort_by {|e| -e[1][:count]}
   end
 
   private
