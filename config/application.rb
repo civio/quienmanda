055a583118ded91e390a70f6a5c28696904c693c
David Cabo <david.cabo@gmail.com>
2013-08-16 19:34:59 +0200

    Try to fix hstore dependency in Travis

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module Quienmanda
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # We need hstore enabled in test database
+    config.active_record.schema_format = :sql
   end
 end
