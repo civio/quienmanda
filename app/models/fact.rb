0784e1a15f36527d3057b2b2f7f777bc3612600d
David Cabo <david.cabo@gmail.com>
2013-08-20 23:15:39 +0200

    Import unprocessed facts in deterministic order (ID ASC). #25

--- app/models/fact.rb
+++ app/models/fact.rb
@@ -1,11 +1,12 @@
 class Fact < ActiveRecord::Base
   has_and_belongs_to_many :relations
 
-  # Return those facts that have not been processed yet, i.e. have no associated relations
+  # Return those facts that have not been processed yet, i.e. have no associated relations,
+  # ordered by fact id (to avoid surprises when reviewing a dry-run import and then doing it)
   def Fact.unprocessed_facts
     # Rails 4 requires the .references bit
     # See http://blog.remarkablelabs.com/2012/12/what-s-new-in-active-record-rails-4-countdown-to-2013
-    Fact.includes(:relations).where('relation_id IS NULL').references(:relations)
+    Fact.includes(:relations).where('relation_id IS NULL').references(:relations).order('facts.id ASC')
   end
 
   def summary
