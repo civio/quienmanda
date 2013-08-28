8ff368cacb73ab03b752d8c3315cbabb867f5bf1
David Cabo <david.cabo@gmail.com>
2013-08-28 19:19:37 +0200

    Add a simple slider to frontpage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    # Show the latest three articles in the frontpage
+    @frontpage = Post.where("photo is not null").order("updated_at desc").limit(3)
+    @frontpage = @frontpage.published() unless can? :manage, Post
   end
 end
