f974a3a63835f9148d065f9d3c8beb87b161e3c0
David Cabo <david.cabo@gmail.com>
2013-08-28 19:43:04 +0200

    Show some articles in front page. Basic frontpage ready

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,11 @@
 class WelcomeController < ApplicationController
   def index
-    # Show the latest three articles in the frontpage
-    @frontpage = Post.where("photo is not null").order("updated_at desc").limit(3)
-    @frontpage = @frontpage.published() unless can? :manage, Post
+    # Highlight the latest three articles in the frontpage
+    @highlights = Post.where("photo is not null").order("updated_at desc").limit(3)
+    @highlights = @highlights.published() unless can? :manage, Post
+
+    # Show the normal posts also
+    @posts = (can? :manage, Post) ? Post.all : Post.published
+    @posts = @posts.order("updated_at DESC").page(params[:page]).per(6)
   end
 end
