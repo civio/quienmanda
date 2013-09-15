40560e4024d40a0369b5a3e5901fcc5e917606c1
David Cabo <david.cabo@gmail.com>
2013-09-15 20:33:42 +0200

    Hello World network visualization in place #80

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -13,10 +13,32 @@ class PeopleController < ApplicationController
   def show
     authorize! :read, @person
     @relations = (can? :manage, Entity) ? @person.relations : @person.relations.published
+    @graph_data = generate_graph_data(@relations)
   end
 
   private
     def set_person
       @person = Entity.people.find_by_slug(params[:id])
+    end
+
+    # Very early times for this. Will eventually be refactored somewhere else
+    def generate_graph_data(relations)
+      nodes = {}
+      links = []
+      relations.each do |relation|
+        if nodes[relation.source.id].nil?
+          nodes[relation.source.id] = { name: relation.source.name, group: 1, node_id: nodes.size }
+        end
+        if nodes[relation.target.id].nil?
+          nodes[relation.target.id] = { name: relation.target.name, group: 1, node_id: nodes.size }
+        end
+
+        links << { 
+          source: nodes[relation.source.id][:node_id],
+          target: nodes[relation.target.id][:node_id],
+          value: 9
+        }
+      end
+      { nodes: nodes.values, links: links }
     end
 end
