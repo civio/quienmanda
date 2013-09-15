03b8c544099c9615dc57c82d034203694e265ab6
David Cabo <david.cabo@gmail.com>
2013-09-15 21:33:43 +0200

    Hello World network visualization in place (missed files!) #80

---
+++ app/assets/javascripts/viz/network-graph.js
@@ -0,0 +1,52 @@
+function NetworkGraph() {
+
+  /* TODO: Clean hardcoded dimensions below */
+  var width = 960,
+      height = 500;
+
+  var color = d3.scale.category20();
+
+  var force = d3.layout.force()
+      .charge(-200)
+      .linkDistance(120)
+      .size([width, height]);
+
+  var svg = d3.select("#viz-container").append("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+  this.display = function(graph) {
+    force
+        .nodes(graph.nodes)
+        .links(graph.links)
+        .start();
+
+    var link = svg.selectAll(".link")
+        .data(graph.links)
+      .enter().append("line")
+        .attr("class", "link")
+        .style("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+    var node = svg.selectAll(".node")
+        .data(graph.nodes)
+      .enter().append("circle")
+        .attr("class", "node")
+        .attr("r", 20)
+        .style("fill", function(d) { return color(d.group); })
+        .call(force.drag);
+
+    node.append("title")
+        .text(function(d) { return d.name; });
+
+    force.on("tick", function() {
+      link.attr("x1", function(d) { return d.source.x; })
+          .attr("y1", function(d) { return d.source.y; })
+          .attr("x2", function(d) { return d.target.x; })
+          .attr("y2", function(d) { return d.target.y; });
+
+      node.attr("cx", function(d) { return d.x; })
+          .attr("cy", function(d) { return d.y; });
+    });
+  };
+
+};
