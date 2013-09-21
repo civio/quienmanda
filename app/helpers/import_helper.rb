b4fdcfa1da4e12551bb1a88b3d6bd5d13bd404a4
David Cabo <david.cabo@gmail.com>
2013-09-21 18:33:35 +0200

    Warn about some of the apparently successful matches

--- app/helpers/import_helper.rb
+++ app/helpers/import_helper.rb
@@ -10,9 +10,29 @@ module ImportHelper
   end
 
   # Return the CSS class matching the severity/confidence of a given match
+  # TODO: Test this!
   def match_severity(import_name, matched_entity, score)
-    matched_entity ? 
-      (score==-1 ? 'warning' : 'success') : 
-      'error'
+    # If there's no match, the result is clear...
+    return 'error' if matched_entity.nil?
+
+    # Also if the entity was created during import, needs double checking...
+    return 'warning' if score == -1
+
+    # When we have an apparently successful match is when we want to pay more
+    # attention sometimes, since the fuzzy matching can be a bit too agressive.
+    # Remove accented characters and downcase...
+    source = import_name.to_ascii.downcase
+    target = matched_entity.short_or_long_name.to_ascii.downcase
+    # We could check both short _and_ long name, but don't think would add anything
+
+    # ...and then mark as ok the obvious cases.
+    # At this point we could do more sofisticated stuff, like converting the strings
+    # into 'bags of words' and checking whether one is included in the other (allowing
+    # thus for words out of order, or additional middle names), or return success if
+    # score > .9 for example, but this is a trivial approximation that works well.
+    if target.include? source or source.include? target
+      return 'success'
+    end
+    return 'warning'  # Be careful with this match
   end
 end
