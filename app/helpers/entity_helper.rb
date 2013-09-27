855500de1b250c9ef8399a5e0ed42e101f66bd8c
David Cabo <david.cabo@gmail.com>
2013-09-28 01:17:45 +0200

    Show sources in relation tables

--- app/helpers/entity_helper.rb
+++ app/helpers/entity_helper.rb
@@ -4,4 +4,11 @@ module EntityHelper
       link_to("<i class='icon-fontello-#{icon_name}'></i>".html_safe, url, target: '_blank')
     end
   end
+
+  def display_source(via)
+    return if via.blank?
+    link_to via do
+      '<i class="icon-ok"></i>'.html_safe
+    end
+  end
 end
