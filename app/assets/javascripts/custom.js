43b4fa946eff3da7321f6a70b111a1062ef90d68
David Cabo <david.cabo@gmail.com>
2013-09-28 18:21:24 +0200

    Remove unused parts of template JS

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,106 +1,5 @@
 (function($) {
 
-/* TODO: This is only part of the original template JS */
-
-/* -------------------- Check Browser --------------------- */
-
-function browser() {
-  
-  //var isOpera = !!(window.opera && window.opera.version);  // Opera 8.0+
-  //var isFirefox = testCSS('MozBoxSizing');                 // FF 0.8+
-  var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
-      // At least Safari 3+: "[object HTMLElementConstructor]"
-  var isChrome = !isSafari && testCSS('WebkitTransform');  // Chrome 1+
-  //var isIE = /*@cc_on!@*/false || testCSS('msTransform');  // At least IE6
-
-  function testCSS(prop) {
-      return prop in document.documentElement.style;
-  }
-  
-  if (isSafari || isChrome) {
-    
-    return true;
-    
-  } else {
-    
-    return false;
-    
-  }
-  
-}
-
-/* -------------------- Search --------------------- */
-
-jQuery(document).ready(function($){
-  
-  $('.search > :input').on('keyup',function(){
-    
-    $(this).attr('class', 'activeSearch');
-
-    var count;
-    var timeToEnd = 1000;
-
-    $(':input').keydown(function(){
-
-      clearTimeout(count);
-
-      count = setTimeout(endCount, timeToEnd);
-
-    });
-
-  });
-
-  function endCount(){
-
-    $('.search > :input').attr('class','search-form');
-
-  }
-
-});
-
-/* -------------------- Buttons 3D Style --------------------- */
-
-jQuery(document).ready(function($){
-
-  $(':button').each(function(){
-
-    if($(this).hasClass('btn-tabula')) {
-
-      $(this).wrap('<div class="btn-overlay" />');
-
-      var inner = $(this).html();
-      
-      if(browser()) {
-
-        $(this).html('<span>' + inner + '</span>');
-        
-      } 
-
-    }
-
-  });
-  
-    
-  $('a').each(function(){
-
-    if($(this).hasClass('btnOverlay')) {
-      
-      $(this).wrap('<div class="btn-overlay" />');
-
-      var inner = $(this).html();
-      
-      if(browser()) {
-
-        $(this).html('<span>' + inner + '</span>');
-        
-      } 
-
-    }
-
-  });
-
-});
-
 /* ------------------ Back To Top ------------------- */
 jQuery(document).ready(function($){
 
@@ -110,16 +9,6 @@ jQuery(document).ready(function($){
   });
 
 }); 
-
-/* ------------------ Tooltips ----------------- */
-
-jQuery(document).ready(function($) {
-
-    $('.tooltips').tooltip({
-      selector: "a[rel=tooltip]"
-    })
-
-});
 
 /* -------------------- Isotope --------------------- */
 /* dcabo: This was called on ready() in the original template, but images overlapped
