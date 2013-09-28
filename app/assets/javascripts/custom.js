19b8f8f7ffcaa4f8b3c146d91b0c57805d5dbdf1
David Cabo <david.cabo@gmail.com>
2013-09-28 18:17:38 +0200

    Clean whitespace

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -133,21 +133,20 @@ jQuery(document).ready(function () {
   $('#wall').imagesLoaded(function() {
     
     var $container = $('#wall');
-      $select = $('#filters select');
+    $select = $('#filters select');
 
     // initialize Isotope
     $container.isotope({
-    // options...
-    resizable: false, // disable normal resizing
-    // set columnWidth to a percentage of container width
+      // options...
+      resizable: false, // disable normal resizing
+      // set columnWidth to a percentage of container width
       masonry: { columnWidth: $container.width() / 12 }
     });
 
     // update columnWidth on window resize
     $(window).smartresize(function(){
-    
       $container.isotope({
-      // update columnWidth to a percentage of container width
+        // update columnWidth to a percentage of container width
         masonry: { columnWidth: $container.width() / 12 }
       });
     });
@@ -158,33 +157,31 @@ jQuery(document).ready(function () {
     });
 
     $select.change(function() {
-      
       var filters = $(this).val();
-
-        $container.isotope({
-          filter: filters
-        });
-      
+      $container.isotope({
+        filter: filters
       });
+    });
 
-      var $optionSets = $('#filters .option-set'),
+    var $optionSets = $('#filters .option-set'),
         $optionLinks = $optionSets.find('a');
 
-        $optionLinks.click(function(){
-      
-        var $this = $(this);
-        // don't proceed if already selected
-        if ( $this.hasClass('selected') ) {
-            return false;
-        }
+    $optionLinks.click(function(){      
+      var $this = $(this);
+      // don't proceed if already selected
+      if ( $this.hasClass('selected') ) {
+          return false;
+      }
+
       var $optionSet = $this.parents('.option-set');
       $optionSet.find('.selected').removeClass('selected');
       $this.addClass('selected');
 
       // make option object dynamically, i.e. { filter: '.my-filter-class' }
       var options = {},
-        key = $optionSet.attr('data-option-key'),
-        value = $this.attr('data-option-value');
+          key = $optionSet.attr('data-option-key'),
+          value = $this.attr('data-option-value');
+
       // parse 'false' as false boolean
       value = value === 'false' ? false : value;
       options[ key ] = value;
@@ -197,9 +194,7 @@ jQuery(document).ready(function () {
       }
 
       return false;
-      
       });
-    
   });
   
 });
