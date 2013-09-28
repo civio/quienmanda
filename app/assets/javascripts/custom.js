d9983f7cda9f1874b2af529fe221d11b64ce38a7
David Cabo <david.cabo@gmail.com>
2013-09-28 18:31:37 +0200

    Remove template JS code related to Isotope. We won't have filters

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -21,14 +21,12 @@ function layoutPhotoWall(container_name) {
   $(container_name).imagesLoaded(function() {
     
     var $container = $(container_name);
-    $select = $('#filters select');
-
-    // initialize Isotope
     $container.isotope({
       // options...
       resizable: false, // disable normal resizing
       // set columnWidth to a percentage of container width
-      masonry: { columnWidth: $container.width() / 12 }
+      masonry: { columnWidth: $container.width() / 12 },
+      itemSelector : '.item'
     });
 
     // update columnWidth on window resize
@@ -38,51 +36,6 @@ function layoutPhotoWall(container_name) {
         masonry: { columnWidth: $container.width() / 12 }
       });
     });
-
-
-    $container.isotope({
-      itemSelector : '.item'
-    });
-
-    $select.change(function() {
-      var filters = $(this).val();
-      $container.isotope({
-        filter: filters
-      });
-    });
-
-    var $optionSets = $('#filters .option-set'),
-        $optionLinks = $optionSets.find('a');
-
-    $optionLinks.click(function(){      
-      var $this = $(this);
-      // don't proceed if already selected
-      if ( $this.hasClass('selected') ) {
-          return false;
-      }
-
-      var $optionSet = $this.parents('.option-set');
-      $optionSet.find('.selected').removeClass('selected');
-      $this.addClass('selected');
-
-      // make option object dynamically, i.e. { filter: '.my-filter-class' }
-      var options = {},
-          key = $optionSet.attr('data-option-key'),
-          value = $this.attr('data-option-value');
-
-      // parse 'false' as false boolean
-      value = value === 'false' ? false : value;
-      options[ key ] = value;
-      if ( key === 'layoutMode' && typeof changeLayoutMode === 'function' ) {
-        // changes in layout modes need extra logic
-        changeLayoutMode( $this, options )
-      } else {
-        // otherwise, apply new options
-        $container.isotope( options );
-      }
-
-      return false;
-      });
   });  
 }
 
