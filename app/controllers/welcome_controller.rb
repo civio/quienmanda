65221f8a903fb2b55a99db5cadf63ce2fd328491
David Cabo <david.cabo@gmail.com>
2013-10-12 21:47:40 +0200

    Optimize ETag validation in Welcome controller (and prefetch some children objects, unrelated) #140

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,17 +3,20 @@ class WelcomeController < ApplicationController
 
   def index    
     # Highlight manually curated articles in the frontpage
-    @highlights = Post.where(featured: true).order("updated_at desc")
+    @highlights = Post.where(featured: true).includes(:photo).order("updated_at desc")
     @highlights = @highlights.published() unless can? :manage, Post
 
     # Show the latests posts...
     @posts = (can? :manage, Post) ? Post.all : Post.published
-    @posts = @posts.where(featured: false).order("updated_at DESC").limit(6)
+    @posts = @posts.where(featured: false).includes(:photo).order("updated_at DESC").limit(6)
 
     # ...and photos
     @photos = (can? :manage, Photo) ? Photo.all : Photo.published
     @photos = @photos.order("updated_at DESC").limit(6)
  
-    fresh_when [@highlights, @posts, @photos], public: current_user.nil?
+    fresh_when  last_modified: [@highlights.maximum(:updated_at), 
+                                @posts.maximum(:updated_at), 
+                                @photos.maximum(:updated_at)].max, 
+                public: current_user.nil?
   end
 end
