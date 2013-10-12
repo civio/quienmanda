05e4b921aa6bc99dd9814c4f8384f17dc0e8e805
David Cabo <david.cabo@gmail.com>
2013-10-12 19:26:55 +0200

    Add ETag support for Post show page, part of #140

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,41 +25,43 @@ class PostsController < ApplicationController
   # GET /posts/1.json
   def show
     authorize! :read, @post
-    @title = @post.title
+    if stale?(@post, :public => current_user.nil?)
+      @title = @post.title
 
-    # Get related entities and posts from content
-    # Note: I used to check that we weren't leaking unpublished related articles 
-    # or entities at this point, but it makes no sense: if there's a mention in the
-    # article content (i.e. a link in the text) we are leaking whether we show
-    # the mentionee in the sidebar or not.
-    @related_entities = []
-    @related_posts = []
-    @post.mentions_in_content.each do |mention|
-      mentionee = mention.mentionee
-      if mentionee.class.name == 'Entity'
-        @related_entities << mentionee
-      elsif mentionee.class.name == 'Post'
-        @related_posts << mentionee
+      # Get related entities and posts from content
+      # Note: I used to check that we weren't leaking unpublished related articles 
+      # or entities at this point, but it makes no sense: if there's a mention in the
+      # article content (i.e. a link in the text) we are leaking whether we show
+      # the mentionee in the sidebar or not.
+      @related_entities = []
+      @related_posts = []
+      @post.mentions_in_content.each do |mention|
+        mentionee = mention.mentionee
+        if mentionee.class.name == 'Entity'
+          @related_entities << mentionee
+        elsif mentionee.class.name == 'Post'
+          @related_posts << mentionee
+        end
+        # No point in keeping track of mentioned photos, I think.
       end
-      # No point in keeping track of mentioned photos, I think.
+
+      # Add the other related posts, the ones that are linking to this one post.
+      # We need to check we are not leaking unpublished content at this point.
+      @post.related_posts.each do |post|
+        next unless post.published? or can? :manage, @post
+        @related_posts << post unless @related_posts.include? post
+      end
+
+      # Sort related entities so they are displayed properly in sidebar
+      @related_entities.sort_by! &:priority
+
+      # Parse shortcodes
+      @content = Shortcodes.shortcode(@post.content)
+
+      # Facebook Open Graph metadata
+      @fb_description = @post.lead unless @post.lead.blank?
+      @fb_image_url = @post.photo.file.url(:full) unless @post.photo.nil? or @post.photo.file.nil?
     end
-
-    # Add the other related posts, the ones that are linking to this one post.
-    # We need to check we are not leaking unpublished content at this point.
-    @post.related_posts.each do |post|
-      next unless post.published? or can? :manage, @post
-      @related_posts << post unless @related_posts.include? post
-    end
-
-    # Sort related entities so they are displayed properly in sidebar
-    @related_entities.sort_by! &:priority
-
-    # Parse shortcodes
-    @content = Shortcodes.shortcode(@post.content)
-
-    # Facebook Open Graph metadata
-    @fb_description = @post.lead unless @post.lead.blank?
-    @fb_image_url = @post.photo.file.url(:full) unless @post.photo.nil? or @post.photo.file.nil?
   end
 
   private
