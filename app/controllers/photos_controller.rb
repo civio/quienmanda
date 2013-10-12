bc6814dea0a8d5f0d12d8f06e722504aa3b7a6d0
David Cabo <david.cabo@gmail.com>
2013-10-12 20:40:50 +0200

    Add ETag support for Photo controller #140

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,14 +1,8 @@
 class PhotosController < ApplicationController
-  caches_action :index, 
-                expires_in: 1.hour, 
-                unless: :current_user, 
-                cache_path: Proc.new { request.url + (params[:page]||'') }
-  caches_action :show, 
-                expires_in: 1.hour, 
-                unless: :current_user
-
   before_action :set_title
   before_action :set_photo, only: [:show]
+
+  etag { can? :manage, Entity } # Don't cache admin content together with the rest
 
   layout proc { |controller| controller.request.params[:widget].blank? ? 'application' : 'widget' }
 
@@ -16,23 +10,26 @@ class PhotosController < ApplicationController
   # GET /photos.json
   def index
     @photos = (can? :manage, Photo) ? Photo.all : Photo.published
-    @photos = @photos.order("updated_at DESC").page(params[:page]).per(15)
+    if stale?(@photos, :public => current_user.nil?)
+      @photos = @photos.order("updated_at DESC").page(params[:page]).per(15)
+    end
   end
 
   # GET /photos/1
   # GET /photos/1.json
   def show
     authorize! :read, @photo
+    if stale?(@photo, :public => current_user.nil?)
+      # Set response headers to allow widget being embedded cross-domain
+      # See http://stackoverflow.com/questions/16561066/ruby-on-rails-4-app-not-works-in-iframe
+      if params[:widget]
+        response.headers['X-Frame-Options'] = 'ALLOWALL'
+      end
 
-    # Set response headers to allow widget being embedded cross-domain
-    # See http://stackoverflow.com/questions/16561066/ruby-on-rails-4-app-not-works-in-iframe
-    if params[:widget]
-      response.headers['X-Frame-Options'] = 'ALLOWALL'
+      # Facebook Open Graph metadata
+      @fb_description = @photo.footer unless @photo.footer.blank?
+      @fb_image_url = @photo.file.url(:full) unless @photo.file.nil?
     end
-
-    # Facebook Open Graph metadata
-    @fb_description = @photo.footer unless @photo.footer.blank?
-    @fb_image_url = @photo.file.url(:full) unless @photo.file.nil?
   end
 
   # GET /photos/tagged/juicy
