860a5c38a1cec8f41e4e19ba1cf40577b7a9e776
David Cabo <david.cabo@gmail.com>
2014-03-23 11:46:08 +0100

    Allow :en locale, for admin pages. Rails 4.0.4 is more strict about this

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,6 @@ module Quienmanda
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :es
-    config.i18n.available_locales = [:es]
+    config.i18n.available_locales = [:es, :en]
   end
 end
