68981cd5fa21dbc4e736bb4ed442f9968d352db4
David Cabo <david.cabo@gmail.com>
2014-03-23 18:34:02 +0100

    Fix RSS feed redirect #141

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -67,7 +67,7 @@ class PostsController < ApplicationController
       respond_to do |format|
         format.atom { render :layout => false }
         # we want the RSS feed to redirect permanently to the ATOM feed
-        format.rss { redirect_to feed_path(:format => :atom), :status => :moved_permanently }
+        format.rss { redirect_to feed_posts_path(:format => :atom), :status => :moved_permanently }
       end
     end
   end
