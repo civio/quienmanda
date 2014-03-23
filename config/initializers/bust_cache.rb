175a17cd1928e3353ab9df462b151ecd3f4e42ad
David Cabo <david.cabo@gmail.com>
2014-03-23 18:43:32 +0100

    Bust cache

--- config/initializers/bust_cache.rb
+++ config/initializers/bust_cache.rb
@@ -1,4 +1,4 @@
 # Force cache busting on front-end/CSS changes. See:
 #  - http://stackoverflow.com/questions/22469591/how-to-expire-rails-conditional-get-caching
 #  - http://ninjasandrobots.com/rails-caching-a-problem-with-etags-and-a-solution
-ENV["RAILS_CACHE_ID"] = '20140323 1828'
+ENV["RAILS_CACHE_ID"] = '20140323 1843'
