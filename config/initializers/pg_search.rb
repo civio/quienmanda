fe38b11a460c4a0db349a61d0ca1bace048832a3
David Cabo <david.cabo@gmail.com>
2014-03-24 20:22:17 +0100

    Configure full-text search to ignore accents. Closes #66

--- config/initializers/pg_search.rb
+++ config/initializers/pg_search.rb
@@ -1,1 +1,7 @@
 # TODO: Configure search. See https://github.com/casecommons/pg_search
+
+PgSearch.multisearch_options = {
+  # Do we want to use :trigram and allow some typos? Not for now
+  # :using => [:tsearch, :trigram],
+  :ignoring => :accents
+}
