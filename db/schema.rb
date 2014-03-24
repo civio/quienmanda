0054cd767f9b12385d55fb10292dc9e1c68506c8
David Cabo <david.cabo@gmail.com>
2014-03-24 20:46:44 +0100

    Add link to configurable url from author name in posts. Fixes #142

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140322193818) do
+ActiveRecord::Schema.define(version: 20140324194109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -196,6 +196,7 @@ ActiveRecord::Schema.define(version: 20140322193818) do
     t.datetime "updated_at"
     t.string   "name",                   default: "",    null: false
     t.boolean  "admin",                  default: false, null: false
+    t.string   "url"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
