fdd1eca75c35f431b838da271a036b7f6936a203
David Cabo <david.cabo@gmail.com>
2014-06-22 18:26:07 +0200

    Bust cache after photo widget changes

--- config/initializers/bust_cache.rb
+++ config/initializers/bust_cache.rb
@@ -1,4 +1,4 @@
 # Force cache busting on front-end/CSS changes. See:
 #  - http://stackoverflow.com/questions/22469591/how-to-expire-rails-conditional-get-caching
 #  - http://ninjasandrobots.com/rails-caching-a-problem-with-etags-and-a-solution
-ENV["RAILS_CACHE_ID"] = '20140323 1843'
+ENV["RAILS_CACHE_ID"] = '20140622 1823'
