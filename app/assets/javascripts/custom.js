ff55fb48320918ab49e08d45355c21afc53a3e8b
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2015-01-23 19:15:42 +0100

    Revert 61c00f2..4436676
    This rolls back to commit 61c00f2c46e2df227c640c88e6a4f1ca885293f7.

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,142 +1,107 @@
 (function($) {
 
-  var $cont, contWidth = 0,
-      hasPhoto = false,
-      hasVis = false,
-      graph;
+/* ------------------ Back To Top ------------------- */
+jQuery(document).ready(function($){
 
-  jQuery(document).ready(function($){
-
-    $cont = $('body > .container');
-    contWidth = $cont.width();
-    
-    hasPhoto = $('#photo').length > 0;        // check if there is a photo entity
-    hasVis = $('#viz-container').length > 0;  // check if there is a visualization
-
-    /* -------------------- Setup layouts --------------------- */
-    layoutPhotoWall('#wall');
-    layoutPhotoWall('.extra-wall'); /* Sigh */
-
-    /* -------------------- Setup visualization --------------- */
-    if( hasVis ){
-      graph = new NetworkGraph("#viz-container", "#infobox");
-      graph.loadNode( $('#viz-container').data('path') );
-      $('#control-fullscreen, #control-fullscreen-exit').click(function() {
-        $('#viz-container').toggleClass('fullscreen');
-        graph.resize();
-        return false;
-      });
-      $('#control-zoom-in').click(function() { graph.zoomIn(); return false; });
-      $('#control-zoom-out').click(function() { graph.zoomOut(); return false; });
-      $('#control-zoom-reset').click(function() { graph.zoomReset(); return false; });
-      $('#control-help').click(function() { return false; });
-      $('#visualization-controls a').tooltip();
-    }
-
-    /* -------------------- Setup Resize --------------------- */
-    onResize();
-    $(window).bind("resize", onResize);
-
-
-    /* -------------------- Misc functions ------------------- */
-
-    // Back to Top
-    $('#under-footer-back-to-top a').click(function(){
-      $('html, body').animate({scrollTop:0}, 300);
-      return false;
-    });
-
-    // Related entities sidebar
-    if ( $('.extra-related-entity').length > 0 ) {
-      $('#related-entities-toggle').click(function(){
-        $('.extra-related-entity').slideToggle();
-        $('#related-entities-toggle').hide();
-      });
-    } else {
-      $('#related-entities-toggle').hide();
-    }
-
-    // Label toggling on/off 
-    $('.label.toggable').click(function() {
-      $(this).toggleClass('active, inactive');
-    });
-
-    // FooTable
-    $('.footable').footable();
-
-    // Newsletter subscription
-    $('#mce-EMAIL, #mce-EMAIL-footer').click(function() {
-      $(this).val('');
-      $(this).css('border', 'none');
-    });
-
+  jQuery('#under-footer-back-to-top a').click(function(){
+    jQuery('html, body').animate({scrollTop:0}, 300); 
+    return false; 
   });
 
+}); 
 
-  /* -------------------- Isotope --------------------- */
-  function layoutPhotoWall(container_name) {
-    $(container_name).imagesLoaded(function() {
-      
-      var $container = $(container_name);
+/* -------------------- Isotope --------------------- */
+function layoutPhotoWall(container_name) {
+  $(container_name).imagesLoaded(function() {
+    
+    var $container = $(container_name);
+    $container.isotope({
+      // options...
+      resizable: false, // disable normal resizing
+      // set columnWidth to a percentage of container width
+      masonry: { columnWidth: $container.width() / 12 },
+      itemSelector : '.item'
+    });
+
+    // update columnWidth on window resize
+    $(window).smartresize(function(){
       $container.isotope({
-        // options...
-        resizable: false, // disable normal resizing
-        // set columnWidth to a percentage of container width
-        masonry: { columnWidth: $container.width() / 12 },
-        itemSelector : '.item'
-      });
-
-      // update columnWidth on window resize
-      $(window).smartresize(function(){
-        $container.isotope({
-          // update columnWidth to a percentage of container width
-          masonry: { columnWidth: $container.width() / 12 }
-        });
+        // update columnWidth to a percentage of container width
+        masonry: { columnWidth: $container.width() / 12 }
       });
     });
-  }
+  });  
+}
 
-  /* -------------------- Resize --------------------- */
-  function onResize(e) {
-    var winHeight = $(window).height();
-    var winWidth = $(window).width();
+jQuery(document).ready(function(){
+  layoutPhotoWall('#wall');
+  layoutPhotoWall('.extra-wall'); /* Sigh */
+});
 
-    // Reset Annotorious when change container width
-    if ($cont && contWidth !== $cont.width()) {
-      contWidth = $cont.width();
 
-      // Reset Annotorious
-      if (hasPhoto) {
-        anno.reset();
-        anno.makeAnnotatable(document.getElementById('photo'));
-      }
+/* -------------------- Width Functions --------------------- */
 
-      // Resize visualization
-      if (hasVis) {
-        graph.resize();
+jQuery(document).ready(function($){
+  
+  widthFunctions();
+
+});
+
+$(window).bind("resize", widthFunctions);
+
+function widthFunctions(e) {
+  var winHeight = $(window).height();
+  var winWidth = $(window).width();
+
+  if (winWidth < 980 && winWidth > 767) {
+    
+    if($("#wall").width()) {
+      if($("#wall.item").hasClass("span3")) {
+        $("#wall.item").removeClass("span3");
+        $("#wall.item").addClass("span4");
+      }      
+    }
+            
+  } else {
+    
+    if($("#wall").width()) {
+      if($(".item").hasClass("span4")) {
+        $("#wall.item").removeClass("span4");
+        $("#wall.item").addClass("span3");
       }
     }
 
-    // Improve this!!!
-    if (winWidth < 980 && winWidth > 767) {
-      
-      if($("#wall").width()) {
-        if($("#wall.item").hasClass("span3")) {
-          $("#wall.item").removeClass("span3");
-          $("#wall.item").addClass("span4");
-        }
-      }
-              
-    } else {
-      
-      if($("#wall").width()) {
-        if($(".item").hasClass("span4")) {
-          $("#wall.item").removeClass("span4");
-          $("#wall.item").addClass("span3");
-        }
-      }
-
-    }
   }
+  
+}
+
+/* -------------------- Misc functions --------------------- */
+
+$(function () {
+
+  // Related entities sidebar
+  if ( $('.extra-related-entity').length > 0 ) {
+    $('#related-entities-toggle').click(function(){
+      $('.extra-related-entity').slideToggle();
+      $('#related-entities-toggle').hide();
+    });
+  } else {
+    $('#related-entities-toggle').hide();
+  };
+
+  // Label toggling on/off 
+  $('.label.toggable').click(function() {
+    $(this).toggleClass('active, inactive');
+  }); 
+
+  // FooTable
+  $('.footable').footable();
+
+  // Newsletter subscription
+  $('#mce-EMAIL, #mce-EMAIL-footer').click(function() {
+    $(this).val('');
+    $(this).css('border', 'none');
+  }); 
+});
 
 })(jQuery);
