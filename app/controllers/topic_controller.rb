a9289ccf9991d85ea887d0d620f4dd84e7ceed9b
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2015-02-18 18:40:04 +0100

    Add Tags into fronted & link it into its related Topic

--- app/controllers/topic_controller.rb
+++ app/controllers/topic_controller.rb
@@ -3,18 +3,22 @@ class TopicController < ApplicationController
   # GET /topic/1
   def show
   	topic = Topic.find_by_slug(params[:id])
-    if stale?(topic, :public => current_user.nil?)
-      	@title = topic.title
-      	@description = topic.description
+    #if stale?(topic, :public => current_user.nil?)
+
+      @title = topic ? topic.title : params[:id].gsub('-', ' ')
+
+      if topic and topic.description.blank?
+        @description = topic.description 
+      end
       	
-	    authorize! :manage, Post
-	    @posts = Post.tagged_with(topic.title.downcase)
+	    authorize! :read, Post
+	    @posts = Post.tagged_with(@title.downcase)
     	@posts = @posts.order("published_at DESC")
 
-    	authorize! :manage, Photo
-	    @photos = Photo.tagged_with(topic.title.downcase)
+    	authorize! :read, Photo
+	    @photos = Photo.tagged_with(@title.downcase)
 	    @photos =  @photos.order("updated_at DESC")
-    end
+    #end
   end
 
 end
