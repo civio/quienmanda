b48aaf9d861d5c04e65e6cfaef9186b7451d7e75
David Cabo <david.cabo@gmail.com>
2015-03-14 16:31:55 +0100

    Fix whitespace

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,12 +2,12 @@ class Topic < ActiveRecord::Base
   belongs_to :entity  # Don't get confused, we _have_ an entity
   belongs_to :photo # Don't get confused, we _have_ a header photo
 
-	validates :title, presence: true, uniqueness: true
+  validates :title, presence: true, uniqueness: true
 
-	acts_as_url :title, url_attribute: :slug, only_when_blank: true
-	def to_param
-	  slug
-	end
+  acts_as_url :title, url_attribute: :slug, only_when_blank: true
+  def to_param
+    slug
+  end
 
   scope :published, -> { where(published: true) }
 end
