ad39880b2345c08a0f5a5b4abb5fa8466fa38c99
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2015-04-27 17:27:30 +0200

    Improve login link & solve problem with logout route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Quienmanda::Application.routes.draw do
   devise_scope :user do
     get 'login' => 'devise/sessions#new', :as => :new_user_session
     post 'login' => 'devise/sessions#create', :as => :user_session
-    get 'logout' => 'devise/sessions#destroy', :as => :destroy_user_session
+    delete 'logout' => 'devise/sessions#destroy', :as => :destroy_user_session
   end
 
   get '/admin/import' => 'import#index', :as => 'import_index'
