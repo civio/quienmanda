037bc1608b08e13571d08e3c44c9d4874c384a17
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2015-10-21 19:06:16 +0200

    Remove Add This
    Closes #266

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Quienmanda::Application.initialize!
+
+# Define Facebook App Id for Facebook Sharing Buttons
+Rails.configuration.facebook_app_id = 1045903075441335
