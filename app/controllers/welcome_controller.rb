fe602d6fb35125728c775ca423d5e3738f5f9942
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2017-11-10 12:26:52 +0100

    Update index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,26 +3,30 @@ class WelcomeController < ApplicationController
   etag { can? :manage, Entity } # Pages seen as admin may look different
 
   def index    
+    # Get topics
+    @topics = (can? :manage, Topic) ? Topic.all : Topic.published
+    fresh_when  etag: @topics
+    @topics = @topics.order("updated_at DESC")
+
     # Highlight manually curated articles in the frontpage
-    @post_highlights = (can? :manage, Post) ? Post.all : Post.published
-    @post_highlights = @post_highlights.where(featured: true).includes(:photo).order("published_at desc").limit(5)
+    #@post_highlights = (can? :manage, Post) ? Post.all : Post.published
+    #@post_highlights = @post_highlights.where(featured: true).includes(:photo).order("published_at desc").limit(5)
 
-    @topic_highlights = (can? :manage, Topic) ? Topic.all : Topic.published
-    @topic_highlights = @topic_highlights.where(featured: true).order("featured_order ASC").limit(4)
+    #@topic_highlights = (can? :manage, Topic) ? Topic.all : Topic.published
+    #@topic_highlights = @topic_highlights.where(featured: true).order("featured_order ASC").limit(4)
 
-    @highlights_items = @post_highlights.size + @topic_highlights.size
+    #@highlights_items = @post_highlights.size + @topic_highlights.size
 
     # Show the latests posts...
-    @posts = (can? :manage, Post) ? Post.all : Post.published
-    @posts = @posts.where(featured: false).includes(:photo).order("published_at DESC").limit(5)
+    #@posts = (can? :manage, Post) ? Post.all : Post.published
+    #@posts = @posts.where(featured: false).includes(:photo).order("published_at DESC").limit(5)
 
     # ...and photos
     @photos = (can? :manage, Photo) ? Photo.all : Photo.published.validated
     @photos = @photos.order("updated_at DESC").limit(6)
  
     # FIXME: Should check that web doesn't break if there're none of them, i.e. without seed data.   
-    fresh_when  etag: [ @highlights,
-                        @posts,
+    fresh_when  etag: [ @topics,
                         @photos]
   end
 end
